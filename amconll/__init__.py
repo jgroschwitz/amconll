c6d02471432dca5e84c2cd6d212b634b8b3a05f1
jgroschwitz <jonasg@coli.uni-saarland.de>
2023-12-16 23:25:03 +0100

    upgrade to version 1.0.2 for PyPI

--- amconll/__init__.py
+++ amconll/__init__.py
@@ -1,1 +1,1 @@
-from .amconll_tools import Entry, AMSentence, write_conll, parse_amconll, from_raw_text
+from .amconll_tools import Entry, AMSentence, write_conll, parse_amconll, from_raw_text, reorder_amconll
